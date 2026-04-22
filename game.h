ac71c0c25a6672cb85f6d11265dd3f2bf3f332a8
Stephen Smith <stephensmith907ak@gmail.com>
2026-04-22 14:47:36 -0500

    Create game.h and game.cpp

---
+++ game.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+
+class Nim
+{
+    private:
+    std::vector<int> piles;
+    int currentPlayer;
+
+    public:
+    //1. board set up
+    //2. move/chat/forfeit checks (also move validity check)
+    //3. decode/encode moves
+    //4. local board update (updates based on whats recieved?)
+    //5. game over check
+    //6. display board function
+    //7. checks for opponent forfeit?
+};
