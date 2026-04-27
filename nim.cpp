211c284284d624afc4400e4267e5cd06303b6aa9
davisjburt <davisb7@me.com>
2026-04-27 14:41:10 -0500

    Refactor main function in nim.cpp to include network initialization and user input handling

--- nim.cpp
+++ nim.cpp
@@ -5,3 +5,37 @@
 #include <string>
 #include "game.h"
 #include "nim.h"
+#include "network.cpp"
+
+
+using namespace std;
+
+int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cout << "WSAStartup failed" << endl;
+        return 1;
+    }
+
+    char myName[MAX_NAME] = "";
+    cout << "Enter your name: ";
+    cin.getline(myName, MAX_NAME);
+
+    if (strlen(myName) == 0) {
+        strcpy_s(myName, MAX_NAME, "Player");
+    }
+
+    cout << "1. Host a game" << endl;
+    cout << "2. Challenge a host" << endl;
+    int choice = getChoice(1, 2);
+
+    if (choice == 1) {
+        hostMode(myName);
+    }
+    else {
+        clientMode(myName);
+    }
+
+    WSACleanup();
+    return 0;
+}
